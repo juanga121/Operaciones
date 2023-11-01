8b3c8e343ff424eca275596ebca5c7f8c40d4c36
Juan Pablo <juanpaj542@gmail.com>
2023-11-01 09:53:03 -0500

    Resta

---
+++ operacionesGIT/trabajo.js
@@ -0,0 +1,16 @@
+function sumar() {
+    var numero1 = document.getElementById("numero1").value;
+    var numero2 = document.getElementById("numero2").value;
+
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+
+    if (isNaN(numero1) || isNaN(numero2)) {
+        document.getElementById("resultado").textContent = "Por favor, ingresa números válidos.";
+    } else {
+        var suma = numero1 + numero2;
+
+        document.getElementById("resultado").textContent = "La suma de " + numero1 + " y " + numero2 + " es igual a: " + suma;
+    }
+}
+
